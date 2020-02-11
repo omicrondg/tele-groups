4b3958437e4e9616d30b066e3ce193a44cca1d1c
Yulia Gurevich <yulia.v.gurevich@gmail.com>
2020-02-11 15:22:04 +0200

    Update

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send("Hello world!"));
 
+app.post('/enable-led', (req, res) => {
+    res.send(req.body);
+});
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 (event, context, callback) => {
