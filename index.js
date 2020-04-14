5632220b45703509837fa133679629573bc330fe
Dmitry Gurevich <dmitry.n.gurevich@gmail.com>
2020-04-14 15:25:27 +0300

    updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const https = require("https");
 // const WebSocketClient = require('ws');
 
 const PORT = process.env.PORT || 5000;
-const BASE_URL = 'https://graph.facebook.com';
+const BASE_URL = 'https://graph.facebook.com/v6.0';
 var corsOptions = {
   origin: "*",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -15,7 +15,7 @@ var corsOptions = {
 
 const app = express();
 app.use(cors(corsOptions));
-app.use(xhub({ algorithm: "sha1", secret: process.env.APP_SECRET }));
+// app.use(xhub({ algorithm: "sha1", secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
 var token =
@@ -32,37 +32,12 @@ app.get("/", function (req, res) {
 });
 
 app.get("/feed", function (req, res) {
-    let query = `${BASE_URL}/${groupId}/feed`;
-  console.log(req);
+    console.log(req);
+    let query = `${BASE_URL}/${groupId}/feed?access_token=${token}`;
+
+    const request = https.get(query);
+    request.end();
   res.send("<pre>" + JSON.stringify(received_updates, null, 2) + "</pre>");
 });
-
-app.post("/facebook", function (req, res) {
-  console.log("Facebook request body:", req.body);
-
-  if (!req.isXHubValid()) {
-    console.log(
-      "Warning - request header X-Hub-Signature not present or invalid"
-    );
-    res.sendStatus(401);
-    return;
-  }
-
-  console.log("request header X-Hub-Signature validated");
-  // Process the Facebook updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
-app.get(['/facebook'], function(req, res) {
-    if (
-      req.query['hub.mode'] == 'subscribe' &&
-      req.query['hub.verify_token'] == token
-    ) {
-      res.send(req.query['hub.challenge']);
-    } else {
-      res.sendStatus(400);
-    }
-  });
 
 app.listen(PORT);
